4a1d1a7a56f765d4a835aaf995354ce72515dc8e
Neeraj Sati <neeraj.sati123@gmail.com>
2023-07-21 09:42:08 +0530

    Register component added

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom';
 
-function Register() {
+function Register({switchPage}) {
+
   return (
-    <div>Register</div>
+        <div className='bg-[#ffdcdc] flex items-center justify-center min-h-screen'>
+            <div className='bg-white py-7 px-10 flex flex-col items-center justify-center max-sm:w-full max-sm:px-5'>
+                <div className='text-[25px] max-sm:text-[20px]'>Register</div>
+                <div className='mt-5 flex flex-col items-start justify-start max-sm:w-full'>
+                    <div className='text-[14px]'>Email</div>
+                    <input className='rounded-md mt-2 py-3 pr-10 px-5 border-2 border-black w-[450px] max-sm:w-full'/>
+                </div>
+                <div className='mt-5 flex flex-col items-start justify-start relative max-sm:w-full'>
+                    <div className='text-[14px]'>Password</div>
+                    <input type="password" className='rounded-md mt-2 py-3 pr-10 px-5 border-2 border-black w-[450px] max-sm:w-full'/>
+                </div>
+                <div className='mt-5 flex flex-col items-start justify-start max-sm:w-full'>
+                    <div className='text-[14px]'>Name</div>
+                    <input className='rounded-md mt-2 py-3 pr-10 px-5 border-2 border-black w-[450px] max-sm:w-full'/>
+                </div>
+                <div className='mt-5 flex flex-col items-start justify-start max-sm:w-full'>
+                    <div className='text-[14px]'>Address</div>
+                    <input className='rounded-md mt-2 py-3 pr-10 px-5 border-2 border-black w-[450px] max-sm:w-full'/>
+                </div>
+                <div className='mt-5 flex flex-col items-start justify-start max-sm:w-full'>
+                    <div className='text-[14px]'>Register as</div>
+                    <select className='rounded-md mt-2 py-3 pr-10 px-5 border-2 border-black w-[450px] max-sm:w-full'>
+                        <option value="Manufacturer" selected>Manufacturer</option>
+                        <option value="Transporter">Transporter</option>
+                    </select>
+                </div>
+                <button className='w-full rounded-sm mt-5 py-2 px-2 bg-[#c1c1ff] text-black'>Register</button>
+                <Link to="login" className='cursor-pointer mt-5 text-[blue]'>Want to log in?</Link>
+            </div>
+        </div>
   )
 }
 
